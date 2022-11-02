3554d822c71e098755c0ac66392f8815320e851b
josephv_2210 <joseph.2210@hotmail.es>
2022-11-01 23:38:45 -0500

    estilos

---
+++ src/pages/menu/home/styles.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    formContent: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '80%',
+      marginTop: '5%'
+    },
+    h1: {
+      fontFamily: 'Montserrat',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginTop: '5%',
+      width: '100%'
+    },
+    h2: {
+      fontFamily: 'Montserrat',
+      fontSize: 15,
+      fontWeight: '500',
+      marginTop: '5%',
+      width: '100%'
+  
+    },
+    formTextInput: {
+        width: '90%',
+        marginTop: '5%'
+    },
+    labelInput: {
+      paddingHorizontal: '2%',
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: '600',
+      color: '#323643'
+    },
+    inputLabel: {
+      marginTop: '5%',
+      
+    },
+    formContainer: {
+      alignItems: 'center',
+      width: '100%',
+    }
+  
+  })
