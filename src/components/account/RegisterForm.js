1748c2e16547c763af98bb21d6e1ed9a57442938
josephv_2210 <joseph.2210@hotmail.es>
2022-11-08 20:09:32 -0500

    functional

---
+++ src/components/account/RegisterForm.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+// import {Input} from 'react-native-element'
+import React from 'react'
+
+export default function RegisterForm() {
+  return (
+    <View>
+      {/* <Input/> */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
--- src/pages/auth/register/Register.js
+++ src/pages/auth/register/Register.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react'
+import styles from './styles';
+import {useNavigation} from '@react-navigation/native'
+import RegisterForm from '../../../components/account/RegisterForm';
 
 const Register = () => {
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.scrollViewContainer}>
+      <Image
+        source={require("../../../assets/resourses/svg/register.png")}
+        resizeMode="contain"
+        style={styles.bkImage}
+      />
+      <View style={[styles.upContainer, styles.shadowProp]}>
+        <Text style={styles.title}>
+          Registrate
+        </Text>
+        <RegisterForm/>
+        {/* <CreateAccount /> */}
+      </View>
     </View>
   )
+
+  function CreateAccount(props) {
+  const navigation = useNavigation();
+
+    return (
+      <Text>
+        <Text
+          onPress={() => console.log("otro metodo")}
+        >Inicia con otro metodo</Text>
+        <Text
+          onPress={() => console.log("olvido su contraseña")}
+        >¿Olvido su contraseña?</Text>
+        <Text
+          onPress={() => navigation.navigate("¿Ya tienes una cuenta?")}
+        >Registrate</Text>
+      </Text>
+    )
+  }
 }
 
 export default Register
