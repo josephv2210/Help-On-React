14e701356d6fce6f25581eaa1966ff9ad0d088b5
josephv_2210 <joseph.2210@hotmail.es>
2022-11-13 17:00:54 -0500

    funcional

--- src/components/account/RegisterForm.js
+++ src/components/account/RegisterForm.js
@@ -1,12 +1,98 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native'
+// import Checkbox from 'expo-checkbox';
+import styles from './styles';
+import gStyles from '../../styles/generalStyles';
 
-export default function RegisterForm() {
-    return (
-        <View>
-            
-        </View>
-    )
+import React, { Component } from 'react'
+
+export default class RegisterForm extends Component {
+
+    state = {
+        nameProyect: '',
+        email: '',
+        password: '',
+        terms: false
+    }
+
+    onSubmitEdit = () => {
+        console.log(this.state)
+    }
+
+    handleNameProyect = (text) => {
+        this.setState({ nameProyect: text })
+    }
+    handleEmail= (text) => {
+        this.setState({ email: text })
+    }
+    handlePassword= (text) => {
+        this.setState({ password: text })
+    }
+    handleTerms= () => {
+        this.setState({ terms: !terms })
+    }
+
+    render() {
+        return (
+            <View>
+                <View style={gStyles.formContainer}>
+                    <View style={gStyles.formContent} >
+
+                        <View style={gStyles.formTextInput}>
+                            <Text style={gStyles.labelInput}>Nombre</Text>
+                            <TextInput
+                                style={gStyles.inputPlaceholder}
+                                placeholder="Nombre"
+                                keyboardType="default"
+                                placeholderTextColor="#B9BCBE"
+                                autoCapitalize="none"
+                                onChangeText={this.handleNameProyect}
+                            />
+                        </View>
+
+                        <View style={gStyles.formTextInput}>
+                            <Text style={gStyles.labelInput}>Correo</Text>
+                            <TextInput
+                                style={gStyles.inputPlaceholder}
+                                placeholder="Correo"
+                                keyboardType="email"
+                                placeholderTextColor="#B9BCBE"
+                                autoCapitalize="none"
+                                onChangeText={this.handleEmail}
+                            />
+                        </View>
+
+                        <View style={gStyles.formTextInput}>
+                            <Text style={gStyles.labelInput}>Contraseña</Text>
+                            <TextInput
+                                style={gStyles.inputPlaceholder}
+                                placeholder="Contraseña"
+                                keyboardType="password"
+                                placeholderTextColor="#B9BCBE"
+                                autoCapitalize="none"
+                                onChangeText={this.handlePassword}
+                                secureTextEntry={true}
+                            />
+                        </View>
+                        
+                        {/* <View style={gStyles.formCheckBox}>
+                            <Text style={gStyles.labelInput}>Contraseña</Text>
+                            <Checkbox
+                                style={gStyles.inputPlaceholder}
+                                onChangeValue={this.handlePassword}
+                                value={this.state.terms}
+                                secureTextEntry={true}
+                                color={isChecked ? '#4630EB' : undefined}
+                            />
+                        </View> */}
+
+
+                        <TouchableHighlight style={gStyles.standarButton} onPress={this.onSubmitEdit}>
+                            <Text style={gStyles.standarButtonLabel}>REGISTRARSE</Text>
+                        </TouchableHighlight>
+
+                    </View>
+                </View>
+            </View>
+        )
+    }
 }
-
-const styles = StyleSheet.create({})
---
+++ src/styles/generalStyles.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    h1: {
+        fontFamily: 'montserrat-bold',
+        fontSize: 20,
+        marginTop: '5%',
+        width: '100%'
+    },
+    h2: {
+        fontSize: 15,
+        fontFamily: 'montserrat-bold',
+        marginTop: '5%',
+        width: '100%'
+
+    },
+
+    formContent: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '80%',
+        marginTop: '5%'
+    },
+    formTextInput: {
+        width: '100%',
+        marginTop: '10%'
+    },
+    labelInput: {
+        fontFamily: 'montserrat-semibold',
+        paddingHorizontal: '2%',
+        fontStyle: 'normal',
+        fontWeight: '600',
+        color: '#323643',
+        fontSize: 15
+    },
+    inputPlaceholder: {
+        marginTop: '5%',
+        backgroundColor: '#FCFCFD',
+        borderWidth: 1,
+        borderColor: '#EFEFEF',
+        borderRadius: 10,
+        padding: '3%',
+        paddingLeft: '10%',
+        marginTop: '3%',
+        fontSize: 13,
+        fontFamily: 'montserrat-semibold',
+    },
+    formContainer: {
+        alignItems: 'center',
+        width: '100%',
+    },
+    standarButton: {
+        marginTop: 25,
+        backgroundColor: '#C96C26',
+        padding:10,
+        borderRadius:5
+    },
+    standarButtonLabel: {
+        fontFamily: 'montserrat-bold',
+        color: '#FFFFFF'
+    },
+    formCheckBox: {
+
+    }
+
+})
