f22091cfa4c968c55857d17c12984344283c0219
DESKTOP-E87HK1O\Felipe <andres.f.21.as@gmail.com>
2022-11-17 22:19:37 -0500

    ChanceHome

--- src/pages/menu/home/Home.js
+++ src/pages/menu/home/Home.js
@@ -1,14 +1,31 @@
-import { View, Text, TextInput,  } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+      <>
+      <Text style={styles.textTitle}>¿A quien quieres ayudar hoy?</Text>
+      <View style={styles.container}>
+        <View style={styles.shearchBar}>
+          <TextInput Value={value} Style={styles.shearchBar} placeholder='Busca y ayuda...'/>
+        </View>
+      </View>
+      </>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  textTitle: {
+    fontFamily: 'montserrat-bold',
+    fontSize: 32,
+    color: '#323643'
+  },
+
+
+})
 
 
 export default Home
